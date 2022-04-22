1617d5d74a64a430b802ff30b1bcf5697e0ba833
Jack Liu <lxzhe718@uw.edu>
2022-04-22 00:52:59 -0700

    firsdt

---
+++ hw3/hw3-q1.sql
@@ -0,0 +1,10 @@
+WITH LongestTime AS
+	(SELECT f1.origin_city AS oc, MAX(f1.actual_time) AS MaxTime
+		FROM FLIGHTS AS f1
+		GROUP BY f1.origin_city)
+SELECT f.origin_city, f.dest_city, f.actual_time AS time
+	FROM FLIGHTS AS f, LongestTime AS LT
+	WHERE f.origin_city = LT.oc
+	AND f.actual_time = LT.MaxTime
+	GROUP BY f.origin_city, f.dest_city
+	ORDER BY f.origin_city ASC, dest_city
