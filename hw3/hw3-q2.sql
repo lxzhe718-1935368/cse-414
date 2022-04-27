a73e3467d70cc7c3059271fbbb56a7f27579f9db
Jack Liu <lxzhe718@uw.edu>
2022-04-26 23:01:14 -0700

    finish

--- hw3/hw3-q2.sql
+++ hw3/hw3-q2.sql
@@ -1,3 +1,26 @@
+-- query returns 109 rows
+-- It took about 8s to run in Azure
+-- First 20 rows:
+-- Aberdeen SD
+-- Abilene TX
+-- Alpena MI
+-- Ashland WV
+-- Augusta GA
+-- Barrow AK
+-- Beaumont/Port Arthur TX
+-- Bemidji MN
+-- Bethel AK
+-- Binghamton NY
+-- Brainerd MN
+-- Bristol/Johnson City/Kingsport TN
+-- Butte MT
+-- Carlsbad CA
+-- Casper WY
+-- Cedar City UT
+-- Chico CA
+-- College Station/Bryan TX
+-- Columbia MO
+-- Columbus GA
 SELECT f.origin_city AS city
 	FROM FLIGHTS AS f
 	WHERE f.canceled = 0
